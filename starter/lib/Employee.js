5a584fd73e501efefcef73fbc6aa3c7cc12ce4c2
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-02-27 14:41:15 +0000

    engineer class expands employee

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,5 +1,6 @@
 // TODO: Write code to define and export the Employee class
 
+
 class Employee {
     constructor(name, id, email){
         this.name = name;
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,2 +1,22 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
+// In addition to Employee's properties and methods, Engineer will also have the following:
+
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+    constructor(name, id, email, github){
+        super(name, id, email);
+        this.github = github;
+    }
+    getGithub(){
+        return this.github;
+    }
+    getRole(){
+        return 'Engineer';
+    }
+}
+
+module.exports = Engineer;
+
+
